2a3159625d58da3eb8b1b90c248ad89fa2c7d553
jhoncalero <jecalerog@gmail.com>
2016-11-22 20:19:36 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+﻿<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Sellos</title>
+<link rel="shortcut icon" href="imagenes/iconosellos.ico">
+<head>
+	<title>Proyecto academias</title>
+</head>
+<style>
+body {
+background-image: url('./imagenes/fondo3.png');
+background-repeat: no-repeat;
+background-attachment: fixed;
+} 
+</style> 
+<br><br><br><br><br><br><br><br>
+<br><br>
+<center>NO TIENE PERMISO</center>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br>
+<center>&nbsp;<table width="90%" cellspacing="2" cellpadding="15" border="1" bordercolor="#1c75bb"> 
+<tr> 
+<td>
+<b><em><font color="red" face="arial, verdana, helvetica" size="2" >Diseñado y administrado por<em> Jhon Calero</em></a></b> 
+</font>	</td></tr></table></center>
